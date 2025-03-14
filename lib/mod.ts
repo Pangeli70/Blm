7e4b60302a366f00190825748ac674b35657c361
PAngeli70 <pangeli70@gmail.com>
2025-03-14 20:38:42 +0100

    Moved A3D exports

--- lib/mod.ts
+++ lib/mod.ts
@@ -9,37 +9,14 @@
  */
 
 
-export * from "../../A3D/lib/classes/ApgA3D_Extrudable.ts";
-
 export * from "./enums/BrdBlm_eBrand.ts";
 export * from "./enums/BrdBlm_eGlossRoughness.ts";
 export * from "./enums/BrdBlm_eMicaceousFinish.ts";
 export * from "./enums/BrdBlm_eProductType.ts";
 
-export * from "../../A3D/lib/enums/ApgA3D_ePlacementOperation.ts";
-export * from "../../A3D/lib/enums/ApgA3D_eTextureType.ts";
-
 export * from "./interfaces/BrdBlm_IMultilanguage.ts";
-
-export * from "../../A3D/lib/interfaces/ApgA3D_IBumpMapDef.ts";
-export * from "../../A3D/lib/interfaces/ApgA3D_ICDNAsset.ts";
-export * from "../../A3D/lib/interfaces/ApgA3D_IIntExtMaterialDef.ts";
-export * from "../../A3D/lib/interfaces/ApgA3D_IIntExtOutlines.ts";
-export * from "../../A3D/lib/interfaces/ApgA3D_IIntExtOutlinesGroups.ts";
-export * from "../../A3D/lib/interfaces/ApgA3D_IMaterialDef.ts";
-export * from "../../A3D/lib/interfaces/ApgA3D_IPlacementOperation.ts";
-export * from "../../A3D/lib/interfaces/ApgA3D_IPoint2D.ts";
-export * from "../../A3D/lib/interfaces/ApgA3D_IPoint3D.ts";
-export * from "../../A3D/lib/interfaces/ApgA3D_IRalColorDef.ts";
-export * from "../../A3D/lib/interfaces/ApgA3D_IRectangle.ts";
-export * from "../../A3D/lib/mod.ts";
-
 
 export * from "./recordsets/BrdBlm_CDNAssets_Recordset.ts";
 export * from "./recordsets/BrdBlm_MicaeousFinishes_MultilangRecordset.ts";
 
-export * from "../../A3D/lib/services/ApgA3D_RalColors_Service.ts";
-
-
-export * as TC from "./_TC/mod.ts";
 
